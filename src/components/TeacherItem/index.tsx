b8b0635e9c5e07e0a00167c1a423160023e9ea22
andregusmao <agusmao720919@gmail.com>
2020-08-06 23:02:54 -0300

    Criação das páginas dinâmicas

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,29 +2,51 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/4572757?s=460&u=6bc4f535b29a5e860ee94b5718dbedb52470c039&v=4" alt="André Gusmão" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>André Gusmão</strong>
-                    <span>Programação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
             <p>
-                Desenvolvedor com mais de 25 anos de experiência em diversas platasformas.
-        </p>
+                {teacher.bio}
+            </p>
             <footer>
                 <p>
-                    Preço por hora: <strong>R$ 80,00</strong>
+                    Preço por hora: <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a href={`https://wa.me/${Number(teacher.whatsapp)}`} onClick={createNewConnection} target="_blank" rel="noopener noreferrer">
                     <img src={whatsappIcon} alt="whatsapp" />
                 Entrar em contato
-            </button>
+            </a>
             </footer>
         </article>
     )
